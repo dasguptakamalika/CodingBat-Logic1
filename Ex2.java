37c286dc8b708d4c9ac7d43e1d086b58d47aa34d
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-08-09 11:20:05 -0400

    Update Ex2.java
    a lil twisted

--- Ex2.java
+++ Ex2.java
@@ -1,19 +1,31 @@
 public int caughtSpeeding(int speed, boolean isBirthday) {
-    int limit1 = 60;
-    int limit2 = 80;
-
+    // On birthday, increase speed limit by 5
     if (isBirthday) {
-        limit1 += 5;
-        limit2 += 5;
+        speed -= 5;
     }
-
-    if (speed <= limit1) {
+    
+    if (speed <= 60) {
         return 0; // no ticket
-    } else if (speed <= limit2) {
+    } else if (speed <= 80) {
         return 1; // small ticket
     } else {
         return 2; // big ticket
     }
 }
+//That means when you reach the else if, the speed >= 60 part is always true, so it’s useless to check it again.
+/*Test 1: caughtSpeeding(60, false)
+isBirthday = false → skip speed -= 5
 
-//A little tough
+speed = 60 → ≤ 60 → return 0
+
+Test 2: caughtSpeeding(65, false)
+isBirthday = false
+
+speed = 65 → falls in >60 && ≤80 → return 1
+
+Test 3: caughtSpeeding(65, true)
+isBirthday = true → speed -= 5 → 65 → 60
+
+speed = 60 → ≤ 60 → return 0
+
+ */
