9062c1f61d71141a2884e19f666ef4acb2a1ebe7
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-08-08 22:52:22 -0400

    Update Ex10.java

--- Ex10.java
+++ Ex10.java
@@ -1,4 +1,13 @@
-package org.Logic1;
+public int blueTicket(int a, int b, int c) {
+    int ab = a + b;
+    int bc = b + c;
+    int ac = a + c;
 
-public class Ex10 {
+    if (ab == 10 || bc == 10 || ac == 10) {
+        return 10;
+    } else if (ab == bc + 10 || ab == ac + 10) {
+        return 5;
+    } else {
+        return 0;
+    }
 }
