0c803017d171e5cffeb70c47616ba032fe37ba3e
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-08-08 22:45:42 -0400

    Update Ex2.java

--- Ex2.java
+++ Ex2.java
@@ -1,4 +1,17 @@
-package org.Logic1;
+public int caughtSpeeding(int speed, boolean isBirthday) {
+    int limit1 = 60;
+    int limit2 = 80;
 
-public class Ex2 {
+    if (isBirthday) {
+        limit1 += 5;
+        limit2 += 5;
+    }
+
+    if (speed <= limit1) {
+        return 0; // no ticket
+    } else if (speed <= limit2) {
+        return 1; // small ticket
+    } else {
+        return 2; // big ticket
+    }
 }
