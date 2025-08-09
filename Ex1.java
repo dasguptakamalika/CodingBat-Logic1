a6f83c0b006295b0bd922c53a7f8ded493fa2581
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-08-08 22:44:47 -0400

    Update Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -1,5 +1,11 @@
-package org.Logic1;
-
-public class Ex1 {
-    
+public boolean cigarParty(int cigars, boolean isWeekend) {
+  if(isWeekend)
+  {
+    return(cigars>=40);
+  }
+  else
+  {
+    return (cigars>=40 && cigars<=60);
+  }
+  
 }
