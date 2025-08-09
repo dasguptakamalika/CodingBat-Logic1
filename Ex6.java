f24a5bffa21c9baf07be4ab8a7123050eb6a39d1
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-08-08 22:47:27 -0400

    Update Ex6.java

--- Ex6.java
+++ Ex6.java
@@ -1,4 +1,9 @@
-package org.Logic1;
-
-public class Ex6 {
+public int teaParty(int tea, int candy) {
+    if (tea < 5 || candy < 5) {
+        return 0; // bad party
+    } else if (tea >= 2 * candy || candy >= 2 * tea) {
+        return 2; // great party
+    } else {
+        return 1; // good party
+    }
 }
