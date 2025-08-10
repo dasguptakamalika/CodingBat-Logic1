3c6ca6fd7637c9f62a73e655ca6c0b73385d4aec
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-08-10 15:47:59 -0400

    Update Ex6.java

--- Ex6.java
+++ Ex6.java
@@ -1,9 +1,12 @@
-public int teaParty(int tea, int candy) {
-    if (tea < 5 || candy < 5) {
-        return 0; // bad party
-    } else if (tea >= 2 * candy || candy >= 2 * tea) {
-        return 2; // great party
-    } else {
-        return 1; // good party
-    }
+public int teaParty(int tea,int candy)
+{
+    if(tea < 5 || candy <5)
+        return 0;
+    else if(tea >= 2 * candy || candy >= 2*tea)
+        return 2;
+    else
+    {
+        return 1;
 }
+
+//    So the “double” rule only matters after passing the first rule (both ≥ 5).
