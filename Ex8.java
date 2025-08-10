e07c9b2fb2c42c894eea15d65117e0fff070d4c1
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-08-10 16:05:13 -0400

    Update Ex8.java

--- Ex8.java
+++ Ex8.java
@@ -1,3 +1,8 @@
-public boolean lastDigit(int a, int b, int c) {
-    return (a % 10 == b % 10 || a % 10 == c % 10 || b % 10 == c % 10);
+public boolean lastDigit(int a, int b, ont c)
+{
+    int al = a%10;
+    int bl =b%10;
+    int cl= c%10;
+
+    return ( al == bl || bl ==cl || cl==al);
 }
