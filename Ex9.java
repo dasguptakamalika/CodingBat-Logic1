279b9161d26776b001c066d9258fdb98615b5be7
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-08-10 16:17:10 -0400

    Update Ex9.java

--- Ex9.java
+++ Ex9.java
@@ -1,9 +1,15 @@
-public int maxMod5(int a, int b) {
-    if (a == b) {
+public int maxMod5(int a, int b)
+{
+    if(a==b)
+    {
         return 0;
-    } else if (a % 5 == b % 5) {
-        return Math.min(a, b);
-    } else {
-        return Math.max(a, b);
     }
+    elseif(a%5==b%5)
+        {
+        return Math.min(a,b);
+    }
+    else
+    {
+       return Math.max(a,b);
+        
 }
