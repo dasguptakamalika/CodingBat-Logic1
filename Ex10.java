a119971b9b1d3506342b378990803f8532e10991
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-08-10 16:24:08 -0400

    Update Ex10.java

--- Ex10.java
+++ Ex10.java
@@ -3,12 +3,9 @@ public int blueTicket(int a, int b, int c) {
     int bc = b + c;
     int ac = a + c;
 
-    if (ab == 10 || bc == 10 || ac == 10) {
-        return 10;
-    } else if (ab == bc + 10 || ab == ac + 10) {
-        return 5;
-    } else {
-        return 0;
-    }
+    if (ab == 10 || bc == 10 || ac == 10) return 10;
+    if (ab == bc + 10 || ab == ac + 10) return 5;
+    return 0;
 }
+
 
