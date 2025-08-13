d32e9eef97ca2d2327dd285a08eb9fc7804bd7fc
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-08-12 23:41:47 -0400

    Add files via upload

---
+++ Ex22.java
@@ -0,0 +1,17 @@
+public String alarmClock(int day, boolean vacation) {
+    if((day >= 1 && day <=5) && vacation)
+        return "10:00";
+    else if((day==0 || day ==6) && vacation)
+        return "off";
+    else if((day>=1 && day <=5) && !vacation)
+        return  "7:00";
+    else if((day ==0 || day==6) && !vacation)
+        return "10:00";
+    else
+    {
+        return "";
+    }
+}
+
+//check out the return type, false (boolean) ≠ "false" (String).
+//Your method must return a String.
