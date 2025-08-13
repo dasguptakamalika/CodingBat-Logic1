a6d47dbec1ca5e605f749145ae250062e0f07e1d
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-08-13 10:30:37 -0400

    Update Ex29.java

---
+++ Ex29.java
@@ -0,0 +1,9 @@
+public int greenTicket(int a, int b, int c) {
+    if (a == b && b == c) {
+        return 20; // all same
+    } else if (a == b || b == c || a == c) {
+        return 10; // exactly two same
+    } else {
+        return 0; // all different
+    }
+}
