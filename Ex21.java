d88adb87e517b38732fb893765bd85cac7ee0835
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-08-12 23:25:23 -0400

    Add files via upload

---
+++ Ex21.java
@@ -0,0 +1,12 @@
+public boolean squirrelPlay(int temp, boolean isSummer) {
+    if (isSummer && temp >= 60 && temp <= 100) {
+        return true;
+    } else if (!isSummer && temp >= 60 && temp <= 90) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+//MUST CHECK FOR THE NON SUMMER ASWELL, KEEP THE NO. OF PARAMETERS SAME AS THE METHOD SIGNATURE
