733bbae7f47f353a8ea3aae51c1cb04d52614066
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-08-14 22:00:16 -0400

    Update Ex30.java
    to access the digits we use valueof to convert to strings

--- Ex30.java
+++ Ex30.java
@@ -6,3 +6,6 @@ public int sumLimit(int a, int b) {
         return a; // sum has more digits
     }
 }
+
+//of course the sum cannot have lesss digits than its numbers right?
+// (Note: one way to compute the number of digits of a non-negative int n is to convert it to a string with String.valueOf(n) and then check the length of the string.)
