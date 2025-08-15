63dad62482111f306f7eae278ea8a08388e295ac
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-08-15 19:24:42 -0400

    Add files via upload

---
+++ Ex33.java
@@ -0,0 +1,6 @@
+public int blackjack(int a, int b) {
+    if (a > 21) a = 0;  // modifies a if it’s over 21
+    if (b > 21) b = 0;  // modifies b if it’s over 21
+
+    return Math.max(a, b);  // always runs last
+}
