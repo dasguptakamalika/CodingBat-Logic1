db7e534b71239b6d770db0c6c3fbb9814f932278
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-08-15 19:41:53 -0400

    Add files via upload

---
+++ Ex34.java
@@ -0,0 +1,9 @@
+public int loneSum(int a, int b, int c) {
+    int sum = 0;
+
+    if (a != b && a != c) sum += a; // a counts only if unique
+    if (b != a && b != c) sum += b; // b counts only if unique
+    if (c != a && c != b) sum += c; // c counts only if unique
+
+    return sum;
+}
