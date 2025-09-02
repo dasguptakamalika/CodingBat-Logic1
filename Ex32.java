d297b5fa3691cec4b8b5ccdb731f70fee33ecca3
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-09-01 23:45:31 -0400

    Update Ex32.java

--- Ex32.java
+++ Ex32.java
@@ -5,9 +5,10 @@ public int noTeenSum(int a, int b, int c) {
 public int fixTeen(int n) {
     if ((n >= 13 && n <= 19) && n != 15 && n != 16) {
         return 0; // bad teen → counts as 0
-    }
+    } 
     return n; // not a bad teen → return it as is
 }
 
 /*see when there is specific exceptions, we ! them out
 VERY IMPORTANT*/
+
