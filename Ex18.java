c184dd917b28602c6e46b4be5085b92758f802f4
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-09-17 11:49:23 -0400

    Update Ex18.java

--- Ex18.java
+++ Ex18.java
@@ -9,7 +9,7 @@ public boolean lessBy10(int a, int b, int c) {
     return Math.abs(a - b) >= 10 || Math.abs(b - c) >= 10 || Math.abs(c - a) >= 10;
 
     if (a >= b + 10 || a <= b - 10)
-        return true;
+        return true; 
     else if (b >= c + 10 || b <= c - 10)
         return true;
     else if (c >= a + 10 || c <= a - 10)
@@ -19,3 +19,4 @@ public boolean lessBy10(int a, int b, int c) {
 }
 
 */
+
