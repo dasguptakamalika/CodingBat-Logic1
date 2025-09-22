41b1467bc80a1c0af44064bf0a0d6d092e4b54c0
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-09-21 23:52:20 -0400

    Update Ex22.java

--- Ex22.java
+++ Ex22.java
@@ -6,7 +6,7 @@ public String alarmClock(int day, boolean vacation) {
     else if((day>=1 && day <=5) && !vacation)
         return  "7:00";
     else if((day ==0 || day==6) && !vacation)
-        return "10:00";
+        return "10:00"; 
     else
     {
         return "";
@@ -16,3 +16,4 @@ public String alarmClock(int day, boolean vacation) {
 //check out the return type, false (boolean) ≠ "false" (String).
 
 //Your method must return a String, always check the return type
+
