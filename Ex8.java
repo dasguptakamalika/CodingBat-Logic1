321c6d0338466af12a3872b0819039138146e0b0
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-09-22 23:53:39 -0400

    Update Ex8.java

--- Ex8.java
+++ Ex8.java
@@ -3,9 +3,10 @@ public boolean lastDigit(int a, int b, int c) {
     int bLast = b % 10;
     int cLast = c % 10;
 
-    return (aLast == bLast) || (aLast == cLast) || (bLast == cLast);
+    return (aLast == bLast) || (aLast == cLast) || (bLast == cLast); 
 }
 /*int num = 347;
 int tens = (num / 10) % 10; 
 System.out.println(tens); // 4   to access 100th and so on
 */
+
