adbd7a0ff7a556d0d62b03a87e76b4791de28a2d
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-10-08 11:15:29 -0400

    Update Ex17.java

--- Ex17.java
+++ Ex17.java
@@ -5,7 +5,7 @@ public boolean lessBy10(int a, int b, int c) {
 
 /*
 Math.abs(a - b) >= 10 doesn’t care which way the difference goes.
-public boolean lessBy10(int a, int b, int c) { 
+public boolean lessBy10(int a, int b, int c) {  
     return Math.abs(a - b) >= 10 || Math.abs(b - c) >= 10 || Math.abs(c - a) >= 10;
 
     if (a >= b + 10 || a <= b - 10)
@@ -19,5 +19,6 @@ public boolean lessBy10(int a, int b, int c) {
 }
 add to GIT
 */
+
 
 
