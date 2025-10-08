eb97ae798b2a46992101670a0d478c2df32514af
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-10-08 11:15:46 -0400

    Update Ex26.java

--- Ex26.java
+++ Ex26.java
@@ -2,7 +2,7 @@ public String fizzString2(int n) {
     if (n % 3 == 0 && n % 5 == 0) {
         return "FizzBuzz!";
     } else if (n % 3 == 0) {
-        return "Fizz!";
+        return "Fizz!"; 
     } else if (n % 5 == 0) {
         return "Buzz!";
     } else {
@@ -13,3 +13,4 @@ public String fizzString2(int n) {
 //&& first = handle the most specific case before the more general ones.
 
 //Otherwise, your more general check would “catch” the number first and skip the special case.
+
