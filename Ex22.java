acf2259bfb19942d199a7e9d2e2117afd0dc3b92
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-11-12 18:27:14 -0500

    Fix formatting and clean up alarmClock method

--- Ex22.java
+++ Ex22.java
@@ -1,7 +1,7 @@
 public String alarmClock(int day, boolean vacation) {
     if((day >= 1 && day <=5) && vacation)
         return "10:00";
-    else if((day==0 || day ==6) && vacation)
+    else if((day==0 || day ==6) && vacation) 
         return "off";
     else if((day>=1 && day <=5) && !vacation)
         return  "7:00";
@@ -16,4 +16,5 @@ public String alarmClock(int day, boolean vacation) {
 //check out the return type, false (boolean) ≠ "false" (String).
 
 //Your method must return a String, always check the return type
+
 
