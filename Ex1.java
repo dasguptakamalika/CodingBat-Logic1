8411b86ef27f376b959488b68b7b2488ce5c0d07
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-11-12 18:19:07 -0500

    Fix formatting of doubleChar method in Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -2,9 +2,10 @@
     StringBuilder result = new StringBuilder();
     for (int i = 0; i < str.length(); i++) {
         char c = str.charAt(i);
-        result.append(c).append(c);
+        result.append(c).append(c); 
     }
     return result.toString();
 }
+
 
 
