73d709b3745616aff66dd6ba53dd05525833baa4
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-11-14 20:52:37 -0500

    Refactor inOrderEqual method for clarity

--- Ex27.java
+++ Ex27.java
@@ -5,9 +5,10 @@ public boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
         return a < b && b < c; // strict increasing
     }
 }
-
+ 
 
 //see how the specific case was handled first
 //also we know how to check if all are equal, a ==b && b==c
 //hence to approve cases like 445 etc , we take this case
 //a <=b && b<=c
+
