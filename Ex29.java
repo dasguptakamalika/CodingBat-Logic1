d097dabcdb69b7edd27a517aa41d6bae12e7b7ca
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-11-14 20:50:49 -0500

    Fix formatting of greenTicket method in Ex29.java

--- Ex29.java
+++ Ex29.java
@@ -1,4 +1,4 @@
-public int greenTicket(int a, int b, int c) {
+ public int greenTicket(int a, int b, int c) {
     if (a == b && b == c) {
         return 20; // all same
     } else if (a == b || b == c || a == c) {
@@ -8,3 +8,4 @@ public int greenTicket(int a, int b, int c) {
     }
 }
  
+
