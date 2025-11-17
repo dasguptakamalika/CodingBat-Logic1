3b2c07607dd15d58e851896f3fdaaab105814054
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-11-16 21:36:19 -0500

    Update Ex34.java

--- Ex34.java
+++ Ex34.java
@@ -3,9 +3,10 @@ public int loneSum(int a, int b, int c) {
 
     if (a != b && a != c) sum += a; // a counts only if unique
     if (b != a && b != c) sum += b; // b counts only if unique
-    if (c != a && c != b) sum += c; // c counts only if unique
+    if (c != a && c != b) sum += c; // c counts only if unique 
 
     return sum;
 }  
+
 
 
