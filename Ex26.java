741e5c21c3f28493427b298d9149f01d67c4d25b
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-11-24 20:25:14 -0500

    Refactor fizzString2 method in Ex26.java

--- Ex26.java
+++ Ex26.java
@@ -3,7 +3,7 @@ public String fizzString2(int n) {
         return "FizzBuzz!";
     } else if (n % 3 == 0) {
         return "Fizz!"; 
-    } else if (n % 5 == 0) {
+    } else if (n % 5 == 0) { 
         return "Buzz!";
     } else {
         return n + "!";
@@ -13,4 +13,5 @@ public String fizzString2(int n) {
 //&& first = handle the most specific case before the more general ones.
 
 //Otherwise, your more general check would “catch” the number first and skip the special case.
+
 
