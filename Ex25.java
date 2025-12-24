c38707012ff23e85c6704e8686da6a31ca73eae9
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-12-24 00:38:26 -0500

    Update Ex25.java

--- Ex25.java
+++ Ex25.java
@@ -5,11 +5,12 @@ public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
     if (isMorning && !isMom) {
         return false; // in morning, only answer if mom
     }
-    return true; // all other cases 
+    return true; // all other cases  
 }
 
 //understand the flow, all cases if ur asllep u aint picking up, then comes next false, if not morning and mom doesnt call, you wont pick up
 //rest of the times , u pick up, also if first if is false then we go to next condition
 //aaaahh dont get it!!!
+
 
 
