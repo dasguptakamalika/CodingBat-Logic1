4e00bbc75eaa1768ed0b3ae4c3d2239e87c7e5d6
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-12-28 23:10:44 -0500

    Update Ex35.java

--- Ex35.java
+++ Ex35.java
@@ -6,11 +6,12 @@ public int round10(int num) {
     int remainder = num % 10;
     if (remainder >= 5) {
         return num + (10 - remainder); // round up
-    } else { 
+    } else {  
         return num - remainder; // round down
     }
 }
 //num = 16, remainder = 6 → (10 - 6) = 4 → 16 + 4 = 20 ✅ rounded up
 
 //num = 25, remainder = 5 → (10 - 5) = 5 → 25 + 5 = 30 ✅ rounded up
+
 
