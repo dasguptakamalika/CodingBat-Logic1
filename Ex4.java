e4547b0db932ee79c2909d392314c3459c73e84c
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2026-02-15 18:31:09 -0500

    Update Ex4.java

--- Ex4.java
+++ Ex4.java
@@ -7,7 +7,7 @@ public boolean more20(int n) {
  
 43 % 20 = 3 → remainder is 3 → ❌ false (it’s +3 from 40, not +1 or +2)
 
-2️⃣ 51
+2️⃣ 51 
 
 51 % 20 = 11 → remainder is 11 → ❌ false (it’s +11 from 40)
 
@@ -21,6 +21,7 @@ public boolean more20(int n) {
  if the number is smaller than the divisor (and can’t be divided even once), then the remainder is just the number itself.
 
 */
+
 
 
 
