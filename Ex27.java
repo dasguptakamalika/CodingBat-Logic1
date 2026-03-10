ce624a1ed7d027a2e3029064329a059d411eb92f
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2026-03-09 22:29:15 -0400

    Update Ex27.java

--- Ex27.java
+++ Ex27.java
@@ -5,12 +5,13 @@ public boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
         return a < b && b < c; // strict increasing
     }
 } 
- 
+  
 
 //see how the specific case was handled first
 //also we know how to check if all are equal, a ==b && b==c  
 //hence to approve cases like 445 etc , we take this case
 //a <=b && b<=c
+
 
 
 
