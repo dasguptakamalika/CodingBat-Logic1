ff72f63b5c5d701f2a13516a7dc850c544d55479
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2026-04-20 22:01:35 -0400

    Refactor lessBy10 method for clarity

--- Ex17.java
+++ Ex17.java
@@ -11,7 +11,7 @@ public boolean lessBy10(int a, int b, int c) {
     if (a >= b + 10 || a <= b - 10)
         return true;
     else if (b >= c + 10 || b <= c - 10)
-        return true;
+        return true; 
     else if (c >= a + 10 || c <= a - 10) 
         return true;
     else
