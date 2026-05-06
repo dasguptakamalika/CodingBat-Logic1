ba7f55e7a97ee48df6865a8e944df94b96ef2ab8
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2026-05-06 09:59:16 -0400

    Update Ex31.java
    concept

--- Ex31.java
+++ Ex31.java
@@ -1,4 +1,4 @@
- public boolean makeBricks(int small, int big, int goal) {
+  public boolean makeBricks(int small, int big, int goal) {
     return (small + big * 5 >= goal && goal % 5 <=small );
 }
 
