09aec9f794d7ebd36dadcd8f3b5ebe1583a98191
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2026-05-15 23:07:33 -0400

    Refactor lessBy10 method for clarity

--- Ex17.java
+++ Ex17.java
@@ -8,7 +8,7 @@ Math.abs(a - b) >= 10 doesn’t care which way the difference goes.
 public boolean lessBy10(int a, int b, int c) {  
     return Math.abs(a - b) >= 10 || Math.abs(b - c) >= 10 || Math.abs(c - a) >= 10;
 
-    if (a >= b + 10 || a <= b - 10)
+    if (a >= b + 10 || a <= b - 10)  
         return true;
     else if (b >= c + 10 || b <= c - 10)
         return true; 
