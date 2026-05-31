4f8069af5ed267da00124aafc24de905c1d67e9f
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2026-05-30 22:07:55 -0400

    Update Ex13.java
    numbers inclusive / exclusive

--- Ex13.java
+++ Ex13.java
@@ -11,7 +11,7 @@ Given a number n and a boolean outsideMode:
   
 If outsideMode is false → Return true if n is between 1 and 10 inclusive
 (n >= 1 && n <= 10),
-otherwise return false.
+otherwise return false. 
 
 If outsideMode is true → Return true if n is less than or equal to 1
 (n <= 1) or greater than or equal to 10 (n >= 10), otherwise return false.*/
